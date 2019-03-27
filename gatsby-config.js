e091d3deaf4086073b4a6237137d3a518e95309c
Grant Garrett <grantgarrett@gmail.com>
2019-03-27 16:31:13 -0700

    Cleaning out starter info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Grant Garrett Design`,
+    description: `Grant Garrett Design`,
+    author: `@grantgarrett`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/pages/invision-case-study.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const InvisionCS = () => (
+  <Layout>
+    <SEO title="Case Study: InVision Inspect Mode" />
+    <h1>InVision Case Study</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default InvisionCS
