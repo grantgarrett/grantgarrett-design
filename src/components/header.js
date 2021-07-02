8653b9bfa8533245658e08350b4b87c53b82d2b7
Grant Garrett <grant@grantgarrett.com>
2021-07-02 12:28:13 -0700

    new tooltip in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,15 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import GLogo from "../images/icons/g-logo"
 //https://www.npmjs.com/package/react-tooltip
 import ReactTooltip from "react-tooltip"
+import Tippy from '@tippyjs/react';
 
 const Header = ({ siteTitle, pageTitle }) => {
   const [emailTooltipText, setEmailTooltipText] = useState("Click to copy my email");
+  const ref = useRef();
+
   return (
     <header>
       <div className="header-wrapper section-wrapper">
@@ -22,19 +25,41 @@ const Header = ({ siteTitle, pageTitle }) => {
         </div>
 
         <div className="header-email">
+
           <button
+            className="email-copy-button"
+            ref={ref}
+            onClick={() => { setEmailTooltipText("Copied!"); navigator.clipboard.writeText("grant@grantgarrett.com") }}
+          >
+            grant@<span>grantgarrett.com</span>
+          </button>
+
+
+
+          {/* <button
             className="email-copy-button"
             onClick={() => { setEmailTooltipText("Copied!"); navigator.clipboard.writeText("grant@grantgarrett.com") }}
             data-tip={emailTooltipText}
             data-for="emailTooltipHeader"
           >
             grant@<span>grantgarrett.com</span>
-          </button>
+          </button> */}
         </div>
-        <ReactTooltip id="emailTooltipHeader" className='tooltip' place="bottom" effect="solid" getContent={() => emailTooltipText} afterShow={() => setEmailTooltipText("Click to copy my email")} />
+        {/* <ReactTooltip id="emailTooltipHeader" className='tooltip' place="bottom" effect="solid" getContent={() => emailTooltipText} afterShow={() => setEmailTooltipText("Click to copy my email")} /> */}
 
       </div>
+      <Tippy
+        content={emailTooltipText}
+        className="tooltip"
+        placement="bottom"
+        reference={ref}
+        hideOnClick={false}
+        offset={[0, 8]}
+        // onUntrigger={() => setEmailTooltipText("Click to copy my email")}
+        onHidden={() => setEmailTooltipText("Click to copy my email")}
+      />
     </header>
+
   )
 }
 
